8f015aecf2498b5988b14eb3961926a4853da767
tedyistiawan <tedypnd07@gmail.com>
2023-04-03 14:43:55 +0700

    today codding 1

--- ActivityStructure_191/ActivityStructure_191.cpp
+++ ActivityStructure_191/ActivityStructure_191.cpp
@@ -17,4 +17,24 @@ int main()
 	mhs1.nama = "Tasya";
 	mhs1.alamat = "semarang";
 	mhs1.umur = 20;
+
+	cout << "\nNim : " << mhs1.nim;
+	cin >> mhs2.nim;
+	cout << "masukan nama: ";
+	cin >> mhs2.nama;
+	cout << "masukan alamat: ";
+	cin >> mhs2.alamat;
+	cout << "masukan umur: ";
+	cin >> mhs2.umur;
+
+	cout << "\nNim : " << mhs1.nim;
+	cout << "\nnama : " << mhs1.nama;
+	cout << "\nalamat : " << mhs1.alamat;
+	cout << "\numur : " << mhs1.umur;
+
+	cout << "\nNim : " << mhs2.nim;
+	cout << "\nnama : " << mhs2.nama;
+	cout << "\nalamat : " << mhs2.alamat;
+	cout << "\numur : " << mhs2.umur;
+
 }
